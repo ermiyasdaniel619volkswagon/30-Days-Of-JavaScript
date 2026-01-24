e7d73c3f53481adfa7100c22f62ed5178d855019
Ermiyas Daniel <ermiyasdaniel619@gmail.com>
2026-01-24 03:17:40 -0800

    Declare variables using let and const with different data types

---
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,20 @@
+//  Declaring different  variables of differnt data type  using let and  const
+
+let firstName = "Ermiyas"; // first name of a person
+let lastName = "Daniel"; // last name of a person
+let country = " Ethiopia"; // country of the  person
+let city = "Addis Ababa"; //capital city
+let Age = "27";
+let isMarried = true; // declaring the marital status
+
+/*NOTE: Using a semicolon at the end of a sentence is not mandatory in JavaScript*/
+
+// Declaring variables with number values using const key word
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let Name = "ermiyas",
+  job = "webDeveloper",
+  live = "Addis Ababa";
