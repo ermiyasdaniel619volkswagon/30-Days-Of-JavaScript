706ca54b5fbff08bd6ca4ff2ad215a042d834e27
Ermiyas Daniel <ermiyasdaniel619@gmail.com>
2026-01-24 03:19:19 -0800

    Display let and const variables

---
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -0,0 +1,4 @@
+// the variable values can be accessed from other variable.js file
+console.log(firstName, lastName, country, city, Age, isMarried);
+console.log(gravity, boilingPoint, PI);
+console.log(Name, job, live);
