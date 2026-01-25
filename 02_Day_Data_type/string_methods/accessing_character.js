cfb8e722d24352418764bc1f56ba8799c7fda1d9
Ermiyas Daniel <ermiyasdaniel619@gmail.com>
2026-01-25 07:12:09 -0800

    learn accessing  character from string

---
+++ 02_Day_Data_type/string_methods/accessing_character.js
@@ -0,0 +1,8 @@
+// accessing characters from string
+let school = "jedan codecademy";
+let number_of_string = school.length;
+console.log(number_of_string);
+let find_n = school[4]; // n
+console.log(find_n);
+let last_string = school.length - 1;
+console.log(last_string);
