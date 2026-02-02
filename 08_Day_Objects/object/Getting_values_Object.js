2e59d686b06000e83610865a26301d5c705898d8
Ermiyas Daniel <ermiyasdaniel619@gmail.com>
2026-02-02 06:41:29 -0800

    Day 8: object fundamentals

---
+++ 08_Day_Objects/object/Getting_values_Object.js
@@ -0,0 +1,27 @@
+const rectangle = {
+  length: 20,
+  width: 20,
+};
+console.log(rectangle.width); // {length: 20, width: 20}
+
+const person = {
+  firstName: "Asabeneh",
+  lastName: "Yetayeh",
+  age: 250,
+  country: "Finland",
+  city: "Helsinki",
+  skills: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node",
+    "MongoDB",
+    "Python",
+    "D3.js",
+  ],
+  isMarried: true,
+};
+console.log(person.country);
+console.log(person.skills);
+console.log(person.skills[5]);
